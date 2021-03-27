b4cf9962afd067767d4785ea17d4a91c512ac5a7
ltnguyen1710 <74306963+ltnguyen1710@users.noreply.github.com>
2021-03-27 15:57:54 +0700

    Update haha.java

--- haha.java
+++ haha.java
@@ -32,7 +32,7 @@ public class SinhVien {
     }
     public String getHoten(){
         return hoten;
-    }
+    }    
     void setMalop (String malop){
         this.malop=malop;
     }
@@ -59,5 +59,5 @@ public class SinhVien {
             return "Đậu";
         return "Rớt";
     }
-    
+   4 
 }
