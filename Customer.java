d4a196e70286e45a1faee229f1b22482d16bffb5
nghiaMeo <73874352+nghiaMeo@users.noreply.github.com>
2021-04-14 12:16:00 +0700

    update customer

--- Customer.java
+++ Customer.java
@@ -1,7 +1,53 @@
 
-package Object;
+package DTO;
+
+import java.util.Date;
 
 public class Customer {
     private String ID,Name,sex;
-    private int age;
+    private Date date;
+    
+
+    public Customer(String ID, String Name, String sex, Date date) {
+        this.ID = ID;
+        this.Name = Name;
+        this.sex = sex;
+        this.date = date;
+    }
+
+    Customer() {
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
 }
