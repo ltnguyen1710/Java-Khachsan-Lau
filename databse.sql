63b5c435a16902a7a1b5d7e16b1379897c3ba48c
holakaka <74349746+holakaka@users.noreply.github.com>
2021-04-22 11:26:27 +0700

    update dtb

--- databse.sql
+++ databse.sql
@@ -22,24 +22,20 @@ create table nhanvien(
 )
 
 create table admin(
-	idadmin int not null primary key,
-	tkadmin varchar(20),
+	tkadmin varchar(20)not null primary key,
 	mkadmin varchar(20)
 )
 drop table admin
 --thêm 1 admin
 select * from admin
-insert into admin values(1,'nguyencop','nghiameow')
+insert into admin values('nguyencop','nghiameow')
 
 --thêm cột ngayvaolam vào bảng nhanvien
 alter table nhanvien add ngayvaolam smalldatetime
 --Thêm tk mk vào bảng nhanvien
-alter table nhanvien add tk varchar(20)
-alter table nhanvien add mk varchar(20)
-
+alter table nhanvien add mk varchar (40)
 --thêm 1 nhân viên để test
 insert into nhanvien values(1,'test','nam',1/1/1111,1/1/1111)
-update nhanvien set tk='cash' where idnhanvien='1'
 update nhanvien set mk='nghiameow' where idnhanvien='1'
 
 
